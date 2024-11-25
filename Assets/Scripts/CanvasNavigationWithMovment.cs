fdf598bffb406a88ad0b4d27b6c88d2d977dddce
Gonzalo-Rod <gonzalo.rodriguez.g@utec.edu.pe>
2024-11-25 12:31:31 -0500

    Avance Canvas

--- Assets/Scripts/CanvasNavigationWithMovment.cs
+++ Assets/Scripts/CanvasNavigationWithMovment.cs
@@ -6,11 +6,13 @@ public class CanvasNavigationWithMovement : MonoBehaviour
     public Canvas canvas1; // First canvas
     public Canvas canvas2; // Second canvas
     public Canvas canvas3; // Third canvas
+    public Canvas canvas4; // Fourth canvas
     public OVRPlayerController playerController; // Reference to OVRPlayerController
 
     public Button buttonOnCanvas1; // Button on the first canvas
     public Button buttonOnCanvas2; // Button on the second canvas
     public Button buttonOnCanvas3; // Button on the third canvas
+    public Button buttonOnCanvas4; // Button on the fourth canvas
 
     void Start()
     {
@@ -18,6 +20,7 @@ public class CanvasNavigationWithMovement : MonoBehaviour
         canvas1.gameObject.SetActive(true);
         canvas2.gameObject.SetActive(false);
         canvas3.gameObject.SetActive(false);
+        canvas4.gameObject.SetActive(false);
 
         // Disable OVRPlayerController movement initially
         DisableMovement();
@@ -25,7 +28,8 @@ public class CanvasNavigationWithMovement : MonoBehaviour
         // Add button listeners
         buttonOnCanvas1.onClick.AddListener(() => SwitchCanvas(canvas1, canvas2));
         buttonOnCanvas2.onClick.AddListener(() => SwitchCanvas(canvas2, canvas3));
-        buttonOnCanvas3.onClick.AddListener(() => EnableMovement());
+        buttonOnCanvas3.onClick.AddListener(() => SwitchCanvas(canvas3, canvas4));
+        buttonOnCanvas4.onClick.AddListener(() => EnableMovement());
     }
 
     void SwitchCanvas(Canvas currentCanvas, Canvas nextCanvas)
